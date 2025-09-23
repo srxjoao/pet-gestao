7396af83d2f456de8589860273ffb230d7ea11e4
Joao Pedro <joao.barbosa6@estudante.ifms.edu.br>
2025-09-23 08:58:25 -0400

    feat: added a new animal

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ const animais: Animal[] = [];
 const pet1 = new Animal(1,"Bob", "Pastor Alemão", false ,"Banho e Tosa")
 animais.push(pet1);
 
+const pet2 = new Animal(2,"Pingo", "Pincher", true, "Consulta de rotina")
+animais.push(pet2)
+
 RelatorioConsulta.gerarRelatorioConsulta(animais);
 
 pet1.registrarConsulta()
+pet2.registrarConsulta()
